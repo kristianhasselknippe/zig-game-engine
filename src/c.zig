2c2fe847658544b226e10d17895ef94fbdf8f6f3
Kristian Hasselknippe <kristian_hasselknippe@outlook.com>
2019-07-05 21:56:48 +0200

    Opened a window

--- src/c.zig
+++ src/c.zig
@@ -1,4 +1,4 @@
 pub use @cImport({
     @cInclude("epoxy/gl.h");
     @cInclude("GLFW/glfw3.h");
-})
+});
--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,56 @@
 const std = @import("std");
 const c = @import("c.zig");
+const debug_gl = @import("debug_gl.zig");
 
-const print = std.debug.warn;
+const debug = std.debug.warn;
+const panic = std.debug.panic;
+
+var window: *c.GLFWwindow = undefined;
+const window_width = 900;
+const window_height = 600;
+
+extern fn errorCallback(err: c_int, description: [*c]const u8) void {
+    panic("Error: {}\n", description);
+}
 
 pub fn main() anyerror!void {
-    print("Hello, world \n");
+    _ = c.glfwSetErrorCallback(errorCallback);
+
+    if (c.glfwInit() == c.GL_FALSE) {
+        panic("GLFW init failure\n");
+    }
+    defer c.glfwTerminate();
+
+    c.glfwWindowHint(c.GLFW_CONTEXT_VERSION_MAJOR, 3);
+    c.glfwWindowHint(c.GLFW_CONTEXT_VERSION_MINOR, 2);
+    c.glfwWindowHint(c.GLFW_OPENGL_FORWARD_COMPAT, c.GL_TRUE);
+    c.glfwWindowHint(c.GLFW_OPENGL_DEBUG_CONTEXT, debug_gl.is_on);
+    c.glfwWindowHint(c.GLFW_OPENGL_PROFILE, c.GLFW_OPENGL_CORE_PROFILE);
+    c.glfwWindowHint(c.GLFW_DEPTH_BITS, 0);
+    c.glfwWindowHint(c.GLFW_STENCIL_BITS, 8);
+    c.glfwWindowHint(c.GLFW_RESIZABLE, c.GL_FALSE);
+
+    window = c.glfwCreateWindow(window_width, window_height, c"Tetris", null, null) orelse {
+        panic("unable to create window\n");
+    };
+
+    const start_time = c.glfwGetTime();
+    var prev_time = start_time;
+
+    while (c.glfwWindowShouldClose(window) == c.GL_FALSE) {
+        c.glClear(c.GL_COLOR_BUFFER_BIT | c.GL_DEPTH_BUFFER_BIT | c.GL_STENCIL_BUFFER_BIT);
+
+        const now_time = c.glfwGetTime();
+        const elapsed = now_time - prev_time;
+        prev_time = now_time;
+
+        //nextFrame(t, elapsed);
+
+        //draw(t, @This());
+        c.glfwSwapBuffers(window);
+
+        c.glfwPollEvents();
+    }
+
+    defer c.glfwDestroyWindow(window);
 }

090795a5e2bd63903204b2947b28bfb0d4332875
Kristian Hasselknippe <kristian@nortero.com>
2019-07-19 18:49:16 +0200

    Added quit by 'q'

--- src/main.zig
+++ src/main.zig
@@ -13,6 +13,28 @@ extern fn errorCallback(err: c_int, description: [*c]const u8) void {
     panic("Error: {}\n", description);
 }
 
+fn Vec3(comptime T: type) type {
+    return struct {
+        x: T,
+        y: T,
+        z: T,
+    };
+}
+
+const Vertex = Vec3(f32);
+
+pub fn init() void {
+    const data = [_]Vertex{
+        Vertex { .x = -1.0, .y = -1.0, .z = 0.0 },
+        Vertex { .x = 1.0, .y = -1.0, .z = 0.0 },
+        Vertex { .x = 0.0, .y = 1.0, .z = 0.0 }
+    };
+
+    var vbo: c.GLuint = undefined;
+    c.glGenVertexArrays(1, &vbo);
+    c.glBindVertexArray(vbo);
+}
+
 pub fn main() anyerror!void {
     _ = c.glfwSetErrorCallback(errorCallback);
 
@@ -37,7 +59,17 @@ pub fn main() anyerror!void {
     const start_time = c.glfwGetTime();
     var prev_time = start_time;
 
-    while (c.glfwWindowShouldClose(window) == c.GL_FALSE) {
+    init();
+
+    var shouldQuit = false;
+
+    while (c.glfwWindowShouldClose(window) == c.GL_FALSE and !shouldQuit) {
+
+        const quitKeyPressed = c.glfwGetKey(window, c.GLFW_KEY_Q);
+        if (quitKeyPressed == c.GLFW_PRESS) {
+            shouldQuit = true;
+        }
+
         c.glClear(c.GL_COLOR_BUFFER_BIT | c.GL_DEPTH_BUFFER_BIT | c.GL_STENCIL_BUFFER_BIT);
 
         const now_time = c.glfwGetTime();

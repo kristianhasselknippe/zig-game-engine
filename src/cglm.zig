6a5b660a42b63a47a14de4732c5b1346dd36fcb2
Kristian Hasselknippe <kristian_hasselknippe@outlook.com>
2019-08-10 16:29:26 +0200

    Got interop with cglm working with malloc (had trouble with c_allocator)

--- src/cglm.zig
+++ src/cglm.zig
@@ -1,23 +1,10 @@
 const c = @import("c.zig");
+const malloc = @import("std").c.malloc;
 const print = @import("std").debug.warn;
+const allocator = @import("std").heap.c_allocator;
 
-pub fn mat4_identity() c.mat4 {
-    var out = c.GLM_MAT4_IDENTITY_INIT;
-    c.glmc_mat4_identity(&out);
-    return out;
-}
-
-pub fn perspective(fovy: f32, aspect: f32, nearVal: f32, farVal: f32) c.mat4 {
-    var out: c.mat4 = [4][4]f32{
-        [4]f32{ 1.0, 0.0, 0.0, 0.0},
-        [4]f32{ 0.0, 1.0, 0.0, 0.0},
-        [4]f32{ 0.0, 0.0, 1.0, 0.0},
-        [4]f32{ 0.0, 0.0, 0.0, 1.0}
-    };
-    print("Hei {} {}\n", out[0][0], out[1][0]);
-    need to align the data
-    c.glmc_perspective(fovy, aspect, nearVal, farVal, &out);
-    print("test");
-    print("Foobar {}\n", out);
+pub fn perspective(fovy: f32, aspect: f32, nearVal: f32, farVal: f32) *c.mat4 {
+    var out = @ptrCast(*c.mat4, @alignCast(16, (malloc(@sizeOf(c.mat4)))));
+    c.glmc_perspective(fovy, aspect, nearVal, farVal, &out[0]);
     return out;
 }
